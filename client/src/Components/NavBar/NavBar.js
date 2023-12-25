96f4ceb1b36cc4853bc824f5a1db2ae9dd2289b4
daniele-farriciello <daniele.farriciello.03@gmail.com>
2023-12-25 19:06:42 +0000

    Navigation bar updated

--- client/src/Components/NavBar/NavBar.js
+++ client/src/Components/NavBar/NavBar.js
@@ -26,7 +26,7 @@ export default function NavBar() {
     };
 
     const navigateToStoryVivenzioGym = () => {
-        navigate('/storyVivenzioGym')
+        navigate('/storyVivenzioGymPage')
     };
 
     const scrollToSponsor = () => {
@@ -37,45 +37,75 @@ export default function NavBar() {
         document.getElementById('contattiSection')?.scrollIntoView({ behavior: 'smooth' });
     };
 
+    const isHomePage = location.pathname === '/';
 
 
     return (
-        <Box >
-            <CssBaseline />
-            <AppBar sx={{ backgroundColor: alpha(dashboardTheme.palette.customColors.white, 0.85) }} component="nav">
+        <>
+            {isHomePage ?
+                <Box >
+                    <CssBaseline />
+                    <AppBar sx={{ backgroundColor: alpha(dashboardTheme.palette.customColors.white, 0.85) }} component="nav">
 
-                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'row' }}>
-                    <Box display={'flex'} flexDirection={'row'}>
-                        <Box onClick={navigateToStoryVivenzioGym}>
-                            <RegoularH2 marginRight={3} hoverColor={dashboardTheme.palette.secondary.main} borderBottom={`2px solid transparent`} borderBottomHover={`2px solid ${alpha(dashboardTheme.palette.customColors.blue, 0.85)}`} transition={'border-bottom 0.5s ease-in-out'} paddingBottom={0.3}>Chi siamo</RegoularH2>
-                        </Box>
-                        <Box onClick={scrollToSponsor}>
-                            <RegoularH2 marginRight={3} hoverColor={dashboardTheme.palette.secondary.main} borderBottom={`2px solid transparent`} borderBottomHover={`2px solid ${alpha(dashboardTheme.palette.customColors.white, 1.0)}`} transition={'border-bottom 0.5s ease-in-out'} paddingBottom={0.3}>Sponsors</RegoularH2>
-                        </Box>
-                        <Box onClick={scrollToContatti}>
-                            <RegoularH2 marginRight={3} hoverColor={dashboardTheme.palette.secondary.main} borderBottom={`2px solid transparent`} borderBottomHover={`2px solid ${alpha(dashboardTheme.palette.customColors.red, 0.85)}`} transition={'border-bottom 0.5s ease-in-out'} paddingBottom={0.3}>Contatti</RegoularH2>
-                        </Box>
-                    </Box>
-                    <Box sx={{ display: 'flex', alignItems: 'center', ml: -43, cursor: 'pointer' }} onClick={handleLogoAndTextClick}>
-                        <Avatar sx={{ width: 56, height: 56, mr: 2 }} alt="logo" src={logo} />
-                        <TeamVivenzioTitle >TEAM VIVENZIO</TeamVivenzioTitle>
-                    </Box>
+                        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'row' }}>
+                            <Box display={'flex'} flexDirection={'row'}>
+                                <Box onClick={navigateToStoryVivenzioGym}>
+                                    <RegoularH2 marginRight={3} hoverColor={dashboardTheme.palette.secondary.main} borderBottom={`2px solid transparent`} borderBottomHover={`2px solid ${alpha(dashboardTheme.palette.customColors.blue, 0.85)}`} transition={'border-bottom 0.5s ease-in-out'} paddingBottom={0.3}>Chi siamo</RegoularH2>
+                                </Box>
+                                <Box onClick={scrollToSponsor}>
+                                    <RegoularH2 marginRight={3} hoverColor={dashboardTheme.palette.secondary.main} borderBottom={`2px solid transparent`} borderBottomHover={`2px solid ${alpha(dashboardTheme.palette.customColors.white, 1.0)}`} transition={'border-bottom 0.5s ease-in-out'} paddingBottom={0.3}>Sponsors</RegoularH2>
+                                </Box>
+                                <Box onClick={scrollToContatti}>
+                                    <RegoularH2 marginRight={3} hoverColor={dashboardTheme.palette.secondary.main} borderBottom={`2px solid transparent`} borderBottomHover={`2px solid ${alpha(dashboardTheme.palette.customColors.red, 0.85)}`} transition={'border-bottom 0.5s ease-in-out'} paddingBottom={0.3}>Contatti</RegoularH2>
+                                </Box>
+                            </Box>
+                            <Box sx={{ display: 'flex', alignItems: 'center', ml: -43, cursor: 'pointer' }} onClick={handleLogoAndTextClick}>
+                                <Avatar sx={{ width: 56, height: 56, mr: 2 }} alt="logo" src={logo} />
+                                <TeamVivenzioTitle >TEAM VIVENZIO</TeamVivenzioTitle>
+                            </Box>
 
-                    <Box display={'flex'} justifyContent={'flex-end'}>
-                        <Box sx={{ marginRight: '20px' }}>
-                            <Link underline="none" color="inherit" href="https://www.instagram.com/mario__vivenzio/" target="_blank" rel="noopener noreferrer">
-                                <InstagramIcon sx={{ fontSize: '3rem', transition: 'color 0.5s', '&:hover': { color: '#C13584' } }} />
-                            </Link>
-                        </Box>
-                        <Box sx={{ display: { xs: 'none', sm: 'block', marginRight: '10px' } }}>
-                            <Link underline="none" color="inherit" href="https://www.facebook.com/mario.vivenzio.58/" target="_blank" rel="noopener noreferrer">
-                                <FacebookIcon sx={{ fontSize: '3rem', transition: 'color 0.5s', '&:hover': { color: '#3876BF' } }} />
-                            </Link>
-                        </Box>
-                    </Box>
+                            <Box display={'flex'} justifyContent={'flex-end'}>
+                                <Box sx={{ marginRight: '20px' }}>
+                                    <Link underline="none" color="inherit" href="https://www.instagram.com/mario__vivenzio/" target="_blank" rel="noopener noreferrer">
+                                        <InstagramIcon sx={{ fontSize: '3rem', transition: 'color 0.5s', '&:hover': { color: '#C13584' } }} />
+                                    </Link>
+                                </Box>
+                                <Box sx={{ display: { xs: 'none', sm: 'block', marginRight: '10px' } }}>
+                                    <Link underline="none" color="inherit" href="https://www.facebook.com/mario.vivenzio.58/" target="_blank" rel="noopener noreferrer">
+                                        <FacebookIcon sx={{ fontSize: '3rem', transition: 'color 0.5s', '&:hover': { color: '#3876BF' } }} />
+                                    </Link>
+                                </Box>
+                            </Box>
 
-                </Toolbar>
-            </AppBar>
-        </Box>
+                        </Toolbar>
+                    </AppBar>
+                </Box>
+
+                :
+                <Box >
+                    <CssBaseline />
+                    <AppBar sx={{ backgroundColor: alpha(dashboardTheme.palette.customColors.white, 0.85) }} component="nav">
+
+                        <Toolbar sx={{ display: 'flex', justifyContent: 'space-evenly', flexDirection: 'row' }}>
+                            <Box sx={{ marginRight: '20px' }}>
+                                <Link underline="none" color="inherit" href="https://www.instagram.com/mario__vivenzio/" target="_blank" rel="noopener noreferrer">
+                                    <FacebookIcon sx={{ fontSize: '3rem', transition: 'color 0.5s', '&:hover': { color: '#3876BF' } }} />
+                                </Link>
+                            </Box>
+                            <Box sx={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }} onClick={handleLogoAndTextClick}>
+                                <Avatar sx={{ width: 56, height: 56, mr: 2 }} alt="logo" src={logo} />
+                                <TeamVivenzioTitle >TEAM VIVENZIO</TeamVivenzioTitle>
+                            </Box>
+                            <Box sx={{ display: { xs: 'none', sm: 'block', marginRight: '10px' } }}>
+                                <Link underline="none" color="inherit" href="https://www.facebook.com/mario.vivenzio.58/" target="_blank" rel="noopener noreferrer">
+                                    <InstagramIcon sx={{ fontSize: '3rem', transition: 'color 0.5s', '&:hover': { color: '#C13584' } }} />
+                                </Link>
+                            </Box>
+                        </Toolbar>
+                    </AppBar>
+                </Box>
+            }
+
+        </>
     );
 }
