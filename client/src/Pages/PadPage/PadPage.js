1a9aab43c3eb5dddc2cb6fd8957f46a240c19589
daniele-farriciello <daniele.farriciello.03@gmail.com>
2023-12-29 08:27:53 +0000

    padPage Phone adapted

--- client/src/Pages/PadPage/PadPage.js
+++ client/src/Pages/PadPage/PadPage.js
@@ -5,7 +5,7 @@ import { Typography, alpha, useMediaQuery, useTheme } from '@mui/material';
 import { RegoularH2, TeamVivenzioTitle } from '../../Components/Typography/Typography';
 import dashboardTheme from '../../Components/DashboardTheme/DashboardTheme';
 import Video from '../../video/maestro-pao-cleme.mp4';
-import Image from '../../img/SecondPage2.jpg'
+import Image from '../../img/photo-grigia-pao-maestro.jpg'
 
 export default function PadPage() {
   const theme = useTheme();
@@ -60,8 +60,8 @@ export default function PadPage() {
         <Grid
           container
           sx={{
-            marginTop: { xs: '50%', sm: '20%', md: '10%', lg: '5%', xl: '5%' },
-            marginBottom: { xs: '5%', sm: '5%', md: '5%'},
+            marginTop: { xs: '85%', sm: '20%', md: '10%', lg: '5%', xl: '5%' },
+            marginBottom: { xs: '15%', sm: '5%', md: '5%'},
             width: { xs: '90%', sm: '80%', md: '60%', lg: '50%', xl: '40%' },
             height: 'auto',
             display: 'flex',
@@ -69,26 +69,26 @@ export default function PadPage() {
             alignItems: 'center',
             flexDirection: 'column',
             borderRadius: '20px',
-            backgroundColor: alpha(dashboardTheme.palette.primary.main, 0.4),
+            backgroundColor: alpha(dashboardTheme.palette.primary.main, 0.7),
             padding: { xs: '10px', sm: '20px', md: '30px', lg: '40px', xl: '50px' },
           }}
         >
-          <TeamVivenzioTitle color={dashboardTheme.palette.primary.main} fontSize={{ xs: '36px', sm: '42px', md: '48px', lg: '54px', xl: '60px' }}>
+          <TeamVivenzioTitle color={dashboardTheme.palette.secondary.main} fontSize={{ xs: '36px', sm: '42px', md: '48px', lg: '54px', xl: '60px' }}>
             PAO
           </TeamVivenzioTitle>
-          <Typography variant="body1" textAlign={'center'}>
+          <Typography color={dashboardTheme.palette.secondary.light} variant="body1" textAlign={'center'}>
             Il Pao è un attrezzo essenziale per l'allenamento in Muay Thai, utilizzato per migliorare precisione e potenza nei colpi.
             Permette di esercitarsi in sicurezza. Simula situazioni di combattimento reale, essenziale per la pratica tecnica.
           </Typography>
-          <RegoularH2 color={dashboardTheme.palette.primary.main}>Tecniche e Esercizi con il Pao</RegoularH2>
-          <Typography variant="body1" textAlign={'center'}>
+          <RegoularH2 color={dashboardTheme.palette.secondary.main}>Tecniche e Esercizi con il Pao</RegoularH2>
+          <Typography color={dashboardTheme.palette.secondary.light} variant="body1" textAlign={'center'}>
             Gli esercizi con il Pao spaziano da sequenze di base a combinazioni avanzate, adatti a tutti i livelli di esperienza.
             L'istruttore guida i praticanti attraverso vari esercizi, enfatizzando non solo la corretta postura e tecnica, ma anche l'importanza della presenza mentale e del controllo emotivo durante l'allenamento.
           </Typography>
-          <RegoularH2 fontSize={{ xs: '24px', sm: '26px', md: '28px', lg: '30px', xl: '32px' }} color={dashboardTheme.palette.primary.main}>
+          <RegoularH2 fontSize={{ xs: '24px', sm: '26px', md: '28px', lg: '30px', xl: '32px' }} color={dashboardTheme.palette.secondary.main}>
             Il Pao come Strumento di Crescita Spirituale
           </RegoularH2>
-          <Typography variant="body1" textAlign={'center'}>
+          <Typography color={dashboardTheme.palette.secondary.light} variant="body1" textAlign={'center'}>
             Nella Muay Thai, l'allenamento con il Pao è visto come un percorso verso la padronanza di sé, dove mente, corpo e spirito lavorano in armonia.
             La pratica regolare con il Pao insegna ai praticanti a rimanere focalizzati, calmi e presenti, trasformando l'allenamento fisico in un'esperienza meditativa e spiritualmente arricchente.
           </Typography>
