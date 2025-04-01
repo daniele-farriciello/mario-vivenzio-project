e61f4ca28ee5f5b8ff8a44b81dbacf1e466f6881
Daniele Farriciello <dfarriciello@canto.com>
2025-04-01 11:14:56 +0100

    added

--- client/src/Pages/SponsorPage/SponsorData.js
+++ client/src/Pages/SponsorPage/SponsorData.js
@@ -1,6 +1,7 @@
 import condominioForYou from '../../img/sponsor-img/condominioForYou.png'
 import sapImpianti from '../../img/sponsor-img/sapImpianti.jpg'
 import antonioAmmaturo from '../../img/sponsor-img/antonioammaturo.jpg'
+import tailandpartner from '../../img/sponsor-img/tailand-partner.jpg'
 
 export const SponsorData =  [
     {
@@ -10,11 +11,16 @@ export const SponsorData =  [
     },
     {
         id: 1,
+        img: tailandpartner,
+        link: "http://rachanonboxing.com/"
+    },
+    {
+        id: 2,
         img: sapImpianti,
         link: "https://www.sapiimpianti.com/"
     },
     {
-        id: 2,
+        id: 3,
         img: antonioAmmaturo,
         link: "https://www.istituto-santantonio.it/"
     }
